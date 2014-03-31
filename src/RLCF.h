56b3c46429f8f1d1618339b8b416dba31ad5f4b7
Poon Jon <poonjon@gmail.com>
2014-03-31 23:09:26 +0800

    added rlcf

---
+++ src/RLCF.h
@@ -0,0 +1,19 @@
+#ifndef	__RLCF_H__
+#define	__RLCF_H__
+
+#define WREG  0xfe8
+#define BSR   0xfe0
+#define STATUS 0xfd8 //BIT 4 = N, BIT 3 = OV, BIT 2 = Z, BIT 1 = DC, BIT 0 = C
+
+unsigned extern char FSR[];
+extern int PC;
+
+void rlcf(Bytecode *code);
+int check_valid_operands(Bytecode *code);
+int operand2_check(Bytecode *code);
+int normal_operand2(Bytecode *code);
+int default_operand2(Bytecode *code);
+int access_destination_operand2(Bytecode *code);
+int banked_destination_operand2(Bytecode *code);
+
+#endif	// __RLCF_H__
---
+++ test/test_RLCF.c
@@ -0,0 +1,11 @@
+#include "unity.h"
+#include "CException.h"
+#include "Bytecode.h"
+#include "RLCF.h"
+
+void setUp() {}
+void tearDown() {}
+
+void test_rlcf(){
+
+}
